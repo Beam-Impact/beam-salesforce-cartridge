e258811bf5008038e9d2a8ac4976a3d3956b4bb5
Alex Clark <aclark@demandware.com>
2016-03-30 10:44:57 -0400

    eslint other models

--- app_storefront_base/cartridge/models/Product/productPricingModel.js
+++ app_storefront_base/cartridge/models/Product/productPricingModel.js
@@ -1,33 +1,48 @@
 'use strict';
 
-var productPricing = function (obj) {
-	var priceModel = obj.getPriceModel();
+var productPricing = function(obj) {
+    var priceModel = obj.getPriceModel();
 
-	this.isPriceRange = priceModel.isPriceRange();
-	this.decimalPrice = getDecimalPrice(priceModel);
-	this.currency = getCurrency(priceModel);
-	this.formattedPrice = getFormattedPrice(priceModel);
-	this.salePrice = {};
-	this.tierPricing = {};
+    this.isPriceRange = priceModel.isPriceRange();
+    this.decimalPrice = getDecimalPrice(priceModel);
+    this.currency = getCurrency(priceModel);
+    this.formattedPrice = getFormattedPrice(priceModel);
+    this.salePrice = {};
+    this.tierPricing = {};
 };
 
+/**
+ * Retrieves a number representing the price of a product
+ * @param {Object} priceModel - the price model of the product object
+ * @returns {Number} a number representing the price of a product
+ */
 function getDecimalPrice(priceModel) {
-	var price = priceModel.getPrice();
+    var price = priceModel.getPrice();
 
-	return price.available ? price.getDecimalValue().get() : priceModel.getMinPrice().getDecimalValue().get();
+    return price.available ? price.getDecimalValue().get() : priceModel.getMinPrice().getDecimalValue().get();
 }
 
+/**
+ * Retrieves a string representing the currency of a price
+ * @param {Object} priceModel - the price model of the product object
+ * @returns {String} a string representing the currency of a price
+ */
 function getCurrency(priceModel) {
-	var price = priceModel.getPrice();
+    var price = priceModel.getPrice();
 
-	return price.available ? price.getCurrencyCode() : priceModel.getMinPrice().getCurrencyCode();
+    return price.available ? price.getCurrencyCode() : priceModel.getMinPrice().getCurrencyCode();
 }
 
 // TODO: price range stuff
+/**
+ * Retrieves a string representing the price of a product
+ * @param {Object} priceModel - the price model of the product object
+ * @returns {String} a string representing the price of a product
+ */
 function getFormattedPrice(priceModel) {
-	var price = priceModel.getPrice();
+    var price = priceModel.getPrice();
 
-	return price.available ? price.toString() : priceModel.getMinPrice().toString();
+    return price.available ? price.toString() : priceModel.getMinPrice().toString();
 }
 
 
