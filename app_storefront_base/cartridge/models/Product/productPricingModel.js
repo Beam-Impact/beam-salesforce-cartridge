01477e2c27e9e6695efd7c054a690ec35a667982
Ilya Volodin <ivolodin@demandware.com>
2016-04-07 11:04:59 -0400

    Adding base template for Product page

--- app_storefront_base/cartridge/models/Product/productPricingModel.js
+++ app_storefront_base/cartridge/models/Product/productPricingModel.js
@@ -1,54 +1,150 @@
 'use strict';
 
 /**
- * the product merchandizing model
- * @param {Object} obj -product Object.
- * @returns {Object} the product merchandizing model
+ * @constructor
+ * @classdesc Pricing model for a given product
+ * @param {Object} product - API object for a product
+ * @param {Object} promotions - API object for promotions for a current product
+ * @param {Object} currency - Current session currencyCode
+ * @param {Object} currentOptionModel - No idea what this is
  */
-function productPricing(obj) {
-    var priceModel = obj.getPriceModel();
+function productPricing(product, promotions, currency, currentOptionModel) {
+    var priceModel = product.getPriceModel();
 
-    this.isPriceRange = priceModel.isPriceRange();
-    this.decimalPrice = getDecimalPrice(priceModel);
-    this.currency = getCurrency(priceModel);
-    this.formattedPrice = getFormattedPrice(priceModel);
-    this.salePrice = {};
-    this.tierPricing = {};
+    var standardPrice = getStandardPrice();
+    var promotionalPrice = getPromotionalPrice(); // TODO: promotions don't seem to work
+    var rangePrice = getRangePrice();
+    var tieredPrice = getTieredPrice();
+
+    if (promotionalPrice && standardPrice) {
+        return {
+            type: 'promotion',
+            standard: standardPrice,
+            promo: promotionalPrice
+        };
+    }
+    if (rangePrice && rangePrice.min.value !== rangePrice.max.value) {
+        rangePrice.type = 'range';
+        return rangePrice;
+    }
+    if (tieredPrice) {
+        tieredPrice.type = 'tiered';
+        return tieredPrice;
+    }
+
+    standardPrice.type = 'standard';
+    return standardPrice;
+
+    /**
+     * Get standard price for a product
+     * @returns {Object} price object
+     * @private
+     */
+    function getStandardPrice() {
+        if (promotions.length > 0) {
+            var priceBook = getPriceBook(priceModel);
+            if (priceModel.length > 0 && priceModel.getPrice().available) {
+                var priceBookPrice = priceModel.getPriceBookPrice(priceBook.ID);
+                if (priceBookPrice.available && priceBookPrice.getCurrency().getCurrencyCode() === standardPrice.getCurrency().getCurrencyCode()) {
+                    return toPriceModel(priceBookPrice);
+                }
+            }
+        }
+        if (priceModel.getPrice().available) {
+            return toPriceModel(priceModel.getPrice());
+        } else {
+            return toPriceModel(priceModel.minPrice);
+        }
+    }
+
+    /**
+     * Get promotional price for a product
+     * @returns {Object} price object
+     * @private
+     */
+    function getPromotionalPrice() {
+        if (promotions.length > 0) {
+            var promotionalPrice = null;
+            promotions.toArray().forEach(function(promo) {
+                if (promo.getPromotionClass() && promo.getPromotionClass().equals(dw.compaign.Promotion.PROMOTION_CLASS_PRODUCT)) {
+                    if (product.optionProduct) {
+                        promotionalPrice = promo.getPromotionalPrice(product, currentOptionModel ? currentOptionModel : product.getOptionModel); // TODO: remove usage of currentOptionModel
+                    } else {
+                        promotionalPrice = promo.getPromotionalPrice(product);
+                    }
+                }
+            });
+
+            if (promotionalPrice) {
+                return toPriceModel(promotionalPrice);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get range price for a product
+     * @returns {Object} object with min and max pricing
+     * @private
+     */
+    function getRangePrice() {
+        if (product.master || product.variationGroup && priceModel.isPriceRange()) {
+            return {
+                min: toPriceModel(priceModel.minPrice),
+                max: toPriceModel(priceModel.maxPrice)
+            };
+        }
+    }
+
+    /**
+     * Get tiered price for a product
+     * @returns {Object} object with keys containing quantity brakets and prices
+     * @private
+     */
+    function getTieredPrice() {
+        var priceTable = priceModel.getPriceTable();
+        var basePriceQuantity = priceModel.getBasePriceQuantity();
+        var tieredPricing = null;
+        priceTable.getQuantities().toArray().forEach(function(quantity) {
+            if (quantity.compareTo(basePriceQuantity) !== 0) {
+                if (!tieredPricing) {
+                    tieredPrice = {};
+                }
+                tieredPricing[quantity.getValue] = toPriceModel(priceTable.getPrice(quantity));
+            }
+        });
+
+        return tieredPricing;
+    }
 }
 
 /**
- * Retrieves a number representing the price of a product
+ * Return correct price book for a given priceModel
  * @param {Object} priceModel - the price model of the product object
- * @returns {Number} a number representing the price of a product
+ * @returns {Object} priceBook of a given priceModel
  */
-function getDecimalPrice(priceModel) {
-    var price = priceModel.getPrice();
-
-    return price.available ? price.getDecimalValue().get() : priceModel.getMinPrice().getDecimalValue().get();
+function getPriceBook(priceModel) {
+    var priceBook = priceModel.priceInfo.priceBook;
+    while (priceBook.parentPriceBook) {
+        priceBook = priceBook.parentPriceBook ? priceBook.parentPriceBook : priceBook;
+    }
+    return priceBook;
 }
 
 /**
- * Retrieves a string representing the currency of a price
- * @param {Object} priceModel - the price model of the product object
- * @returns {String} a string representing the currency of a price
+ * Convert API price to an object
+ * @param {Object} price - Price object returned from the API
+ * @returns {Object} price formatted as a simple object
  */
-function getCurrency(priceModel) {
-    var price = priceModel.getPrice();
+function toPriceModel(price) {
+    var value = price.available ? price.getDecimalValue().get() : null;
+    var currency = price.available ? price.getCurrencyCode() : null;
 
-    return price.available ? price.getCurrencyCode() : priceModel.getMinPrice().getCurrencyCode();
+    return {
+        value: value,
+        currency: currency,
+        formatted: dw.util.StringUtils.formatMoney(dw.value.Money(value, currency))
+    };
 }
-
-// TODO: price range stuff
-/**
- * Retrieves a string representing the price of a product
- * @param {Object} priceModel - the price model of the product object
- * @returns {String} a string representing the price of a product
- */
-function getFormattedPrice(priceModel) {
-    var price = priceModel.getPrice();
-
-    return price.available ? price.toString() : priceModel.getMinPrice().toString();
-}
-
 
 module.exports = productPricing;
