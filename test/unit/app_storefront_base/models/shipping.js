1d4e4a8cf2d778828bf820637885c0c4990f6647
Evan Chessman <echessman@demandware.com>
2016-09-01 09:39:19 -0400

    [RAP-5039: modify shipping unit test]

--- test/unit/app_storefront_base/models/shipping.js
+++ test/unit/app_storefront_base/models/shipping.js
@@ -96,21 +96,34 @@ describe('Shipping', function () {
 
     it('should set default shipping method when shippingMethodID is supplied', function () {
         var shippingMethodID = '002';
+        var shippingMethod = {
+            description: 'Order received in 2 business days',
+            displayName: '2-Day Express',
+            ID: '002',
+            shippingCost: '$0.00',
+            custom: {
+                estimatedArrivalTime: '2 Business Days'
+            }
+        };
         var spy = sinon.spy(defaultShipment, 'setShippingMethod');
+        spy.withArgs(shippingMethod);
 
         ShippingModel.selectShippingMethod(defaultShipment, shippingMethodID);
 
         assert.isTrue(spy.calledOnce);
+        assert.isTrue(spy.withArgs(shippingMethod).calledOnce);
         defaultShipment.setShippingMethod.restore();
     });
 
     it('should set default shipping method when shippingMethodID is not supplied', function () {
         var shippingMethodID = null;
         var spy = sinon.spy(defaultShipment, 'setShippingMethod');
+        spy.withArgs(null);
 
         ShippingModel.selectShippingMethod(defaultShipment, shippingMethodID);
 
         assert.isTrue(spy.calledOnce);
+        assert.isTrue(spy.withArgs(null).calledOnce);
         defaultShipment.setShippingMethod.restore();
     });
 
@@ -126,11 +139,21 @@ describe('Shipping', function () {
                 }
             }
         ]);
+        var shippingMethod = {
+            description: 'Order received within 7-10 business days',
+            displayName: 'Ground',
+            ID: '001',
+            custom: {
+                estimatedArrivalTime: '7-10 Business Days'
+            }
+        };
         var spy = sinon.spy(defaultShipment, 'setShippingMethod');
+        spy.withArgs(shippingMethod);
 
         ShippingModel.selectShippingMethod(defaultShipment, shippingMethodID, shippingMethods);
 
         assert.isTrue(spy.calledOnce);
+        assert.isTrue(spy.withArgs(shippingMethod).calledOnce);
         defaultShipment.setShippingMethod.restore();
     });
 });
