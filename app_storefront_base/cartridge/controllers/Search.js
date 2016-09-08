e4409ba50d2db76f42b8a808a20867fe9f33b421
Syed Shehroz Hussain (Ozzy) <shussain@demandware.com>
2016-09-08 15:02:18 -0400

    [RAP-5151 : parsed refinements from querystring object]

--- app_storefront_base/cartridge/controllers/Search.js
+++ app_storefront_base/cartridge/controllers/Search.js
@@ -5,6 +5,39 @@ var locale = require('~/cartridge/scripts/middleware/locale');
 var ProductSearchModel = require('dw/catalog/ProductSearchModel');
 
 /**
+ * Creates a plain object of parsed refinements from querystring
+ * @param {Object} querystring - querystring from the local request object
+ * @returns {Object} a plain object of the refinements
+ */
+function parseRefinements(querystring) {
+    var refinements = {};
+    Object.keys(querystring).forEach(function (key) {
+        if (key.indexOf('pref') === 0) {
+            var index = key.substr(5);
+            if (!isNaN(parseInt(index, 10))) {
+                if (key.charAt(4) === 'n') {
+                    if (!refinements[index]) {
+                        refinements[index] = { name: querystring[key] };
+                    } else {
+                        refinements[index].name = querystring[key];
+                    }
+                } else if (key.charAt(4) === 'v') {
+                    if (!refinements[index]) {
+                        refinements[index] = { value: querystring[key] };
+                    } else {
+                        refinements[index].value = querystring[key];
+                    }
+                }
+            }
+        }
+    });
+
+    return Object.keys(refinements).map(function (index) {
+        return refinements[index];
+    });
+}
+
+/**
  * Creates a plain object of the product search model
  * @param {Object} req - local instance of request object
  * @returns {Object} a plain object of the product search model
@@ -13,6 +46,7 @@ function getModel(req) {
     var SearchModel = require('~/cartridge/models/search');
     var productSearchModel = new ProductSearchModel();
     var dataForSearch = {
+        refinements: parseRefinements(req.querystring),
         searchPhrase: req.querystring.q
     };
     return new SearchModel(productSearchModel, dataForSearch);
@@ -22,7 +56,5 @@ server.get('Show', locale, function (req, res, next) {
     res.render('search/searchresult', getModel(req));
     next();
 });
-
-// FIX ME: separate function for formatting refinements from querystring HERE!
 
 module.exports = server.exports();
