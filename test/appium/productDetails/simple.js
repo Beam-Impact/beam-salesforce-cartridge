354939517805beaf15ead7aa86805646b1f65625
Zi Sardone <zsardone@demandware.com>
2016-09-16 15:22:29 -0400

    Appium: removed pause and app define from simple.js

--- test/appium/productDetails/simple.js
+++ test/appium/productDetails/simple.js
@@ -2,28 +2,21 @@
 
 import {assert} from 'chai';
 
-//do "npm install webdriverio" before import
-var webdriverio = require('webdriverio');
-
-describe('Product Item', () => {
+describe('Product Details - Product Item', () => {
     const variantId = 708141677197;
     const basePath = 'en_US/Product-Show/?pid=708141677197';
-    // console.log('url is', config.baseUrl + basePath);
 
     before(() => {
         console.log('[before] Starting...');
         return browser
-            // .init()
             .url('http://dev02-lab03b-dw.demandware.net/on/demandware.store/Sites-SiteGenesis-Site/en_US/Product-Show/?pid=708141677197');
     });
 
     it('should display its product ID', () => {
         console.log('[it] Starting test...');
         return browser
-            // .getText(`#item-variantId .product-number`)
             .getText(`.product-id`)
             .then(itemNumber => {
-                console.log('Item No = ', itemNumber);
                 return assert.equal(itemNumber, variantId);
             })
     });
