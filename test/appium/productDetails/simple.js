36b7cce60ffca1cb869e17a66392986924c9e814
Zi Sardone <zsardone@demandware.com>
2016-09-20 16:54:49 -0400

    Appium:productDetailsTest

--- test/appium/productDetails/simple.js
+++ test/appium/productDetails/simple.js
@@ -6,6 +6,11 @@ import * as productDetailPage from '../../pageObjects/productDetails';
 describe('Product Details - Product Item', () => {
     const variantId = 708141677197;
     const basePath = '/en_US/Product-Show/?pid=708141677197';
+    const expectedPrimaryImage = 'PG.15J0037EJ.SLABLFB.PZ.jpg';
+    const expectedSecondaryImage = 'PG.15J0037EJ.SLABLFB.BZ.jpg';
+    const nextButton = '.right.carousel-control .icon-next';
+    const elementPrimaryImage = '.carousel-item.active .img-fluid';
+    const elementImage = '.img-fluid';
 
     before(() => {
         return browser
@@ -22,8 +27,28 @@ describe('Product Details - Product Item', () => {
 
     it('should display its product name', () =>
         browser.getText(productDetailPage.PRODUCT_NAME)
-            .then(name => assert.equal(name, 'No-Iron Textured Dress Shirt')
-            )
+            .then(name => assert.equal(name, 'No-Iron Textured Dress Shirt'))
     );
 
+    it('should display its product image', () =>
+        browser.isExisting(elementImage)
+            .then(exists => assert.isTrue(exists))
+    );
+
+    it('should display the default variant primary image', () => {
+        return browser.element(elementPrimaryImage)
+            .then(el => browser.elementIdAttribute(el.value.ELEMENT, 'src'))
+            .then(displayedImgSrc => assert.isTrue(displayedImgSrc.value.endsWith(expectedPrimaryImage)));
+    })
+
+    it('should display the secondary default variant primary image after click', () => {
+        return browser.element(nextButton)
+            .click()
+            .waitForVisible(elementImage, 1000, true)
+            .then(() => browser.element(elementPrimaryImage))
+            .then(el => browser.elementIdAttribute(el.value.ELEMENT, 'src'))
+            .then(displayedImgSrc => assert.isTrue(displayedImgSrc.value.endsWith(expectedSecondaryImage)));
+
+
+    })
 })
--- test/pageObjects/productDetails.js
+++ test/pageObjects/productDetails.js
@@ -1,3 +1,4 @@
 'use strict';
 
 export const PRODUCT_NAME = '.product-detail .product-name.hidden-md-up';
+export const PRIMARY_IMAGE = '.carousel-item.active';
