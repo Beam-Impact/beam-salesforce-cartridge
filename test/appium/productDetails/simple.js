eda3767d54c3be5e2db7a840bf1d4db3f22c2d3d
Zi Sardone <zsardone@demandware.com>
2016-09-20 15:47:35 -0400

    Appium:productDetailsTest

--- test/appium/productDetails/simple.js
+++ test/appium/productDetails/simple.js
@@ -1,24 +1,29 @@
 "use strict";
 
 import {assert} from 'chai';
+import * as productDetailPage from '../../pageObjects/productDetails';
 
 describe('Product Details - Product Item', () => {
     const variantId = 708141677197;
-    const basePath = 'en_US/Product-Show/?pid=708141677197';
+    const basePath = '/en_US/Product-Show/?pid=708141677197';
 
     before(() => {
-        console.log('[before] Starting...');
         return browser
-            .url('http://dev02-lab03b-dw.demandware.net/on/demandware.store/Sites-SiteGenesis-Site/en_US/Product-Show/?pid=708141677197');
+            .url(basePath);
     });
 
     it('should display its product ID', () => {
-        console.log('[it] Starting test...');
         return browser
             .getText(`.product-id`)
             .then(itemNumber => {
                 return assert.equal(itemNumber, variantId);
             })
     });
+
+    it('should display its product name', () =>
+        browser.getText(productDetailPage.PRODUCT_NAME)
+            .then(name => assert.equal(name, 'No-Iron Textured Dress Shirt')
+            )
+    );
 
 })
---
+++ test/pageObjects/productDetails.js
@@ -0,0 +1,3 @@
+'use strict';
+
+export const PRODUCT_NAME = '.product-detail .product-name.hidden-md-up';
