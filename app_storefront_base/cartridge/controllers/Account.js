8adc9407a3ee1cc4743c10a94d4ffca83d909bd3
Alex Clark <aclark@demandware.com>
2016-10-24 09:32:54 -0400

    RAP-5027 - registration form

--- app_storefront_base/cartridge/controllers/Account.js
+++ app_storefront_base/cartridge/controllers/Account.js
@@ -12,6 +12,7 @@ var ShippingModel = require('~/cartridge/models/shipping');
 var Totals = require('~/cartridge/models/totals');
 var Transaction = require('dw/system/Transaction');
 var CustomerMgr = require('dw/customer/CustomerMgr');
+var Resource = require('dw/web/Resource');
 var URLUtils = require('dw/web/URLUtils');
 
 /**
@@ -92,6 +93,111 @@ server.post('Login', locale, server.middleware.https, function (req, res, next) 
         res.render('/account/login', {
             navTabValue: 'login',
             loginFormError: true
+        });
+    }
+    next();
+});
+
+server.get('Registration', locale, server.middleware.https, function (req, res, next) {
+    var profileForm = server.forms.getForm('profile');
+
+    // TODO clear form
+    res.render('/account/register', {
+        profileForm: profileForm,
+        navTabValue: 'register'
+    });
+    next();
+});
+
+server.post('SubmitRegistration', locale, server.middleware.https, function (req, res, next) {
+    var registrationForm = server.forms.getForm('profile');
+
+    // form validation
+    if (registrationForm.customer.email.value !== registrationForm.customer.emailconfirm.value) {
+        registrationForm.customer.email.valid = false;
+        registrationForm.customer.emailconfirm.valid = false;
+        registrationForm.customer.emailconfirm.error =
+            Resource.msg('error.message.mistmatch.email', 'forms', null);
+        registrationForm.valid = false;
+    }
+
+    if (registrationForm.login.password.value !== registrationForm.login.passwordconfirm.value) {
+        registrationForm.login.password.valid = false;
+        registrationForm.login.passwordconfirm.valid = false;
+        registrationForm.login.passwordconfirm.error =
+            Resource.msg('error.message.mistmatch.password', 'forms', null);
+        registrationForm.valid = false;
+    }
+
+    // setting variables for the BeforeComplete function
+    var registrationFormObj = {
+        firstName: registrationForm.customer.firstname.value,
+        lastName: registrationForm.customer.lastname.value,
+        email: registrationForm.customer.email.value,
+        emailConfirm: registrationForm.customer.emailconfirm.value,
+        password: registrationForm.login.password.value,
+        passwordConfirm: registrationForm.login.passwordconfirm.value,
+        validForm: registrationForm.valid,
+        form: registrationForm
+    };
+
+    if (registrationForm.valid) {
+        res.setViewData(registrationFormObj);
+
+        this.on('route:BeforeComplete', function (req, res) { // eslint-disable-line no-shadow
+            // getting variables for the BeforeComplete function
+            var registrationForm = res.getViewData(); // eslint-disable-line
+
+            if (registrationForm.validForm) {
+                var login = registrationForm.email;
+                var password = registrationForm.password;
+                var authenticatedCustomer;
+
+                // attempt to create a new user and log that user in.
+                try {
+                    Transaction.wrap(function () {
+                        var newCustomer = CustomerMgr.createCustomer(login, password);
+
+                        if (newCustomer) {
+                            // assign values to the profile
+                            var newCustomerProfile = newCustomer.getProfile();
+                            authenticatedCustomer =
+                                CustomerMgr.loginCustomer(login, password, false);
+                            newCustomerProfile.firstName = registrationForm.firstName;
+                            newCustomerProfile.lastName = registrationForm.lastName;
+                            newCustomerProfile.email = registrationForm.email;
+                        }
+
+                        if (authenticatedCustomer === undefined) {
+                            registrationForm.validForm = false;
+                            registrationForm.form.customer.email.valid = false;
+                            registrationForm.form.customer.emailconfirm.valid = false;
+                        }
+                    });
+                } catch (e) {
+                    registrationForm.validForm = false;
+                    registrationForm.form.customer.email.valid = false;
+                    registrationForm.form.customer.emailconfirm.valid = false;
+                    registrationForm.form.customer.emailconfirm.error =
+                        Resource.msg('error.message.username.taken', 'forms', null);
+                }
+            }
+
+            if (registrationForm.validForm) {
+                res.redirect(URLUtils.url('Account-Show'));
+            } else {
+                res.render('/account/register', {
+                    profileForm: registrationForm.form,
+                    navTabValue: 'register',
+                    registrationFormError: !registrationForm.validForm
+                });
+            }
+        });
+    } else {
+        res.render('/account/register', {
+            profileForm: registrationForm,
+            navTabValue: 'register',
+            registrationFormError: !registrationForm.validForm
         });
     }
     next();
