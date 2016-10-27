7bcc4a932d3100920918fd31da998621860ba5bb
Tyrin Avery <tavery@demandware.com>
2016-10-27 17:31:01 -0400

    DOC-1288: JSDoc now works for MFSG. Fixed jsdoc errors in stores.js. Added readmes.

--- bin/doc.sh
+++ bin/doc.sh
@@ -5,17 +5,16 @@
 # clean up old doc/dist directory
 rm -rf doc/dist && \
     # jsdoc server-side JS
-    #jsdoc . -c doc/js/server/conf.json -d doc/dist/js/server -R app_storefront_controllers/README.md && \
+    jsdoc . -c doc/js/server/conf.json -d doc/dist/js/server -R app_storefront_base/README.md && \
     # jsdoc client-side JS
     #jsdoc . -c doc/js/client/conf.json -d doc/dist/js/client -R app_storefront_base/cartridge/js/README.md && \
     # styleguide
     mkdir -p doc/dist/styleguide && \
-    node-sass --include-path node_modules/bootstrap-sass/assets/stylesheets --include-path node_modules/font-awesome/scss --include-path node_modules/flag-icon-css/sass doc/styleguide/scss/main.scss | postcss -u autoprefixer -o doc/dist/styleguide/main.css && \
-    browserify doc/styleguide/js/main.js -t hbsfy -o doc/dist/styleguide/main.js && \
     # copy static assets
     cp doc/index.html doc/dist/index.html && \
     cp doc/styleguide/index.html doc/dist/styleguide/ && \
     cp -r doc/styleguide/lib/ doc/dist/styleguide/lib && \
-    cp -r doc/styleguide/templates/ doc/dist/styleguide/templates && \
+    cp -r doc/styleguide/templates/ doc/dist/styleguide/templates # && \
     # server the files locally
-    http-server doc/dist -p 5000
+    # http-server doc/dist -p 5000
+
