26ed9d4f868aeda26b49d8bffa3c87bcfdec458b
Zi Sardone <zsardone@salesforce.com>
2017-02-19 17:38:06 -0500

    RAP-5688-saucelabs-readMe

--- test/functional/webdriver/wdio.conf.js
+++ test/functional/webdriver/wdio.conf.js
@@ -27,16 +27,16 @@ if (opts.sauce) {
     sauce.user = process.env.SAUCE_USER;
     sauce.key = process.env.SAUCE_ACCESS_KEY;
     sauce.capabilities = [
-        { browserName: 'chrome', platform: 'OS X 10.10', version: '45.0' }
-        // {
-        //     browserName: 'safari',
-        //     appiumVersion: '1.6.3',
-        //     deviceName: 'iPad Retina Simulator',
-        //     orientation: 'PORTRAIT',
-        //     platformVersion: '10.0',
-        //     platformName: 'iOS',
-        //     app: ''
-        // }
+        { browserName: 'chrome', platform: 'OS X 10.10', version: '45.0' },
+        {
+            browserName: 'safari',
+            appiumVersion: '1.6.3',
+            deviceName: 'iPad Retina Simulator',
+            orientation: 'PORTRAIT',
+            platformVersion: '10.0',
+            platformName: 'iOS',
+            app: ''
+        }
     ];
 }
 
