ca665a28d754ade5c41f092f26aeaca64054b44c
Peter Pritchard <pjpritch@me.com>
2017-03-16 14:52:08 -0400

    RAP-5925: added back payment unit tests and linted

--- test/mocks/models/payment.js
+++ test/mocks/models/payment.js
@@ -1,11 +1,54 @@
 'use strict';
 
 var proxyquire = require('proxyquire').noCallThru().noPreserveCache();
+var helpers = require('../dwHelpers');
 
 function proxyModel() {
     return proxyquire('../../../cartridges/app_storefront_base/cartridge/models/payment', {
-        '~/cartridge/scripts/dwHelpers': {},
-        'dw/order/PaymentMgr': {},
+        '~/cartridge/scripts/dwHelpers': helpers,
+        'dw/order/PaymentMgr': {
+            getApplicablePaymentMethods: function () {
+                return [
+                    {
+                        ID: 'GIFT_CERTIFICATE',
+                        name: 'Gift Certificate'
+                    },
+                    {
+                        ID: 'CREDIT_CARD',
+                        name: 'Credit Card'
+                    }
+                ];
+            },
+            getPaymentMethod: function () {
+                return {
+                    getApplicablePaymentCards: function () {
+                        return [
+                            {
+                                cardType: 'Visa',
+                                name: 'Visa',
+                                UUID: 'some UUID'
+                            },
+                            {
+                                cardType: 'Amex',
+                                name: 'American Express',
+                                UUID: 'some UUID'
+                            },
+                            {
+                                cardType: 'Master Card',
+                                name: 'MasterCard'
+                            },
+                            {
+                                cardType: 'Discover',
+                                name: 'Discover'
+                            }
+                        ];
+                    }
+                };
+            },
+            getApplicablePaymentCards: function () {
+                return ['applicable payment cards'];
+            }
+        },
         'dw/order/PaymentInstrument': {}
     });
 }
--- test/unit/app_storefront_base/models/payment.js
+++ test/unit/app_storefront_base/models/payment.js
@@ -1,115 +1,136 @@
-// TODO: PJP uncomment and fix
-// 'use strict';
+'use strict';
 
-// var assert = require('chai').assert;
+var assert = require('chai').assert;
 
-// var ArrayList = require('../../../mocks/dw.util.Collection');
+var ArrayList = require('../../../mocks/dw.util.Collection');
 
-// var paymentMethods = new ArrayList([
-//     {
-//         ID: 'GIFT_CERTIFICATE',
-//         name: 'Gift Certificate'
-//     },
-//     {
-//         ID: 'CREDIT_CARD',
-//         name: 'Credit Card'
-//     }
-// ]);
+var paymentMethods = new ArrayList([
+    {
+        ID: 'GIFT_CERTIFICATE',
+        name: 'Gift Certificate'
+    },
+    {
+        ID: 'CREDIT_CARD',
+        name: 'Credit Card'
+    }
+]);
 
-// var paymentCards = new ArrayList([
-//     {
-//         cardType: 'Visa',
-//         name: 'Visa',
-//         UUID: 'some UUID'
-//     },
-//     {
-//         cardType: 'Amex',
-//         name: 'American Express',
-//         UUID: 'some UUID'
-//     },
-//     {
-//         cardType: 'Master Card',
-//         name: 'MasterCard'
-//     },
-//     {
-//         cardType: 'Discover',
-//         name: 'Discover'
-//     }
-// ]);
+var paymentCards = new ArrayList([
+    {
+        cardType: 'Visa',
+        name: 'Visa',
+        UUID: 'some UUID'
+    },
+    {
+        cardType: 'Amex',
+        name: 'American Express',
+        UUID: 'some UUID'
+    },
+    {
+        cardType: 'Master Card',
+        name: 'MasterCard'
+    },
+    {
+        cardType: 'Discover',
+        name: 'Discover'
+    }
+]);
 
-// var paymentInstruments = new ArrayList([
-//     {
-//         creditCardNumberLastDigits: '1111',
-//         creditCardHolder: 'The Muffin Man',
-//         creditCardExpirationYear: 2018,
-//         creditCardType: 'Visa',
-//         maskedCreditCardNumber: '************1111',
-//         paymentMethod: 'CREDIT_CARD',
-//         creditCardExpirationMonth: 1,
-//         paymentTransaction: {
-//             amount: {
-//                 value: 0
-//             }
-//         }
-//     },
-//     {
-//         giftCertificateCode: 'someString',
-//         maskedGiftCertificateCode: 'some masked string',
-//         paymentMethod: 'GIFT_CERTIFICATE',
-//         paymentTransaction: {
-//             amount: {
-//                 value: 0
-//             }
-//         }
-//     }
-// ]);
+var paymentInstruments = new ArrayList([
+    {
+        creditCardNumberLastDigits: '1111',
+        creditCardHolder: 'The Muffin Man',
+        creditCardExpirationYear: 2018,
+        creditCardType: 'Visa',
+        maskedCreditCardNumber: '************1111',
+        paymentMethod: 'CREDIT_CARD',
+        creditCardExpirationMonth: 1,
+        paymentTransaction: {
+            amount: {
+                value: 0
+            }
+        }
+    },
+    {
+        giftCertificateCode: 'someString',
+        maskedGiftCertificateCode: 'some masked string',
+        paymentMethod: 'GIFT_CERTIFICATE',
+        paymentTransaction: {
+            amount: {
+                value: 0
+            }
+        }
+    }
+]);
 
-// describe('Payment', function () {
-//     var PaymentModel = require('../../../mocks/models/payment');
+function createApiBasket(options) {
+    var basket = {
+        totalGrossPrice: {
+            value: 'some value'
+        }
+    };
 
-//     it('should take payment Methods and convert to a plain object ', function () {
-//         var result = new PaymentModel(paymentMethods, null, null);
-//         assert.equal(
-//             result.applicablePaymentMethods.length, 1
-//         );
-//         assert.equal(result.applicablePaymentMethods[0].ID, 'CREDIT_CARD');
-//         assert.equal(result.applicablePaymentMethods[0].name, 'Credit Card');
-//     });
+    if (options && options.paymentMethods) {
+        basket.paymentMethods = options.paymentMethods;
+    }
 
-//     it('should take payment cards and convert to a plain object ', function () {
-//         var result = new PaymentModel(null, paymentCards, null);
-//         assert.equal(
-//             result.applicablePaymentCards.length, 4
-//         );
-//         assert.equal(result.applicablePaymentCards[0].cardType, 'Visa');
-//         assert.equal(result.applicablePaymentCards[0].name, 'Visa');
-//         assert.equal(result.applicablePaymentCards[1].cardType, 'Amex');
-//         assert.equal(result.applicablePaymentCards[1].name, 'American Express');
-//     });
+    if (options && options.paymentCards) {
+        basket.paymentCards = options.paymentCards;
+    }
 
-//     it('should take payment instruments and convert to a plain object ', function () {
-//         var result = new PaymentModel(null, null, paymentInstruments);
-//         assert.equal(
-//             result.selectedPaymentInstruments.length, 2
-//         );
-//         assert.equal(result.selectedPaymentInstruments[0].lastFour, '1111');
-//         assert.equal(result.selectedPaymentInstruments[0].owner, 'The Muffin Man');
-//         assert.equal(result.selectedPaymentInstruments[0].expirationYear, 2018);
-//         assert.equal(result.selectedPaymentInstruments[0].type, 'Visa');
-//         assert.equal(
-//             result.selectedPaymentInstruments[0].maskedCreditCardNumber,
-//             '************1111'
-//         );
-//         assert.equal(result.selectedPaymentInstruments[0].paymentMethod, 'CREDIT_CARD');
-//         assert.equal(result.selectedPaymentInstruments[0].expirationMonth, 1);
-//         assert.equal(result.selectedPaymentInstruments[0].amount, 0);
+    if (options && options.paymentInstruments) {
+        basket.paymentInstruments = options.paymentInstruments;
+    }
 
-//         assert.equal(result.selectedPaymentInstruments[1].giftCertificateCode, 'someString');
-//         assert.equal(
-//             result.selectedPaymentInstruments[1].maskedGiftCertificateCode,
-//             'some masked string'
-//         );
-//         assert.equal(result.selectedPaymentInstruments[1].paymentMethod, 'GIFT_CERTIFICATE');
-//         assert.equal(result.selectedPaymentInstruments[1].amount, 0);
-//     });
-// });
+    return basket;
+}
+
+describe('Payment', function () {
+    var PaymentModel = require('../../../mocks/models/payment');
+
+    it('should take payment Methods and convert to a plain object ', function () {
+        var result = new PaymentModel(createApiBasket({ paymentMethods: paymentMethods }), null);
+        assert.equal(
+            result.applicablePaymentMethods.length, 1
+        );
+        assert.equal(result.applicablePaymentMethods[0].ID, 'CREDIT_CARD');
+        assert.equal(result.applicablePaymentMethods[0].name, 'Credit Card');
+    });
+
+    it('should take payment cards and convert to a plain object ', function () {
+        var result = new PaymentModel(createApiBasket({ paymentCards: paymentCards }), null);
+        assert.equal(
+            result.applicablePaymentCards.length, 4
+        );
+        assert.equal(result.applicablePaymentCards[0].cardType, 'Visa');
+        assert.equal(result.applicablePaymentCards[0].name, 'Visa');
+        assert.equal(result.applicablePaymentCards[1].cardType, 'Amex');
+        assert.equal(result.applicablePaymentCards[1].name, 'American Express');
+    });
+
+    it('should take payment instruments and convert to a plain object ', function () {
+        var result = new PaymentModel(createApiBasket({ paymentInstruments: paymentInstruments }), null);
+        assert.equal(
+            result.selectedPaymentInstruments.length, 2
+        );
+        assert.equal(result.selectedPaymentInstruments[0].lastFour, '1111');
+        assert.equal(result.selectedPaymentInstruments[0].owner, 'The Muffin Man');
+        assert.equal(result.selectedPaymentInstruments[0].expirationYear, 2018);
+        assert.equal(result.selectedPaymentInstruments[0].type, 'Visa');
+        assert.equal(
+            result.selectedPaymentInstruments[0].maskedCreditCardNumber,
+            '************1111'
+        );
+        assert.equal(result.selectedPaymentInstruments[0].paymentMethod, 'CREDIT_CARD');
+        assert.equal(result.selectedPaymentInstruments[0].expirationMonth, 1);
+        assert.equal(result.selectedPaymentInstruments[0].amount, 0);
+
+        assert.equal(result.selectedPaymentInstruments[1].giftCertificateCode, 'someString');
+        assert.equal(
+            result.selectedPaymentInstruments[1].maskedGiftCertificateCode,
+            'some masked string'
+        );
+        assert.equal(result.selectedPaymentInstruments[1].paymentMethod, 'GIFT_CERTIFICATE');
+        assert.equal(result.selectedPaymentInstruments[1].amount, 0);
+    });
+});
