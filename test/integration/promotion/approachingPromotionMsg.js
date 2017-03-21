e195bc9469b3a13932791330dd9128457cbfa91b
Zi Sardone <zsardone@salesforce.com>
2017-03-20 23:28:02 -0400

    RAP-5933-approachingOrderLevelPromo

---
+++ test/integration/promotion/approachingPromotionMsg.js
@@ -0,0 +1,95 @@
+
+var assert = require('chai').assert;
+var request = require('request-promise');
+var config = require('../it.config');
+var cheerio = require('cheerio');
+
+// Test Case
+// pid=799927767720
+// 1. Product PDP page contains the promotional messages
+// 2. Add 2 products to Cart, should return approaching order/shipping promotion messages
+// 3. shipping cost and order discount
+// 4. Shipping form updates
+
+describe('Approaching order level promotion', function () {
+    this.timeout(5000);
+
+    var masterPid = '25594767';
+    var variantPid = '799927767720';
+    var qty = 2;
+    var cookieString;
+    var cookieJar = request.jar();
+    var myShippingMethodForm = {
+        methodID: '001'
+    };
+
+    var myRequest = {
+        url: '',
+        method: 'GET',
+        rejectUnauthorized: false,
+        resolveWithFullResponse: true,
+        jar: cookieJar
+    };
+
+    var orderDiscountMsg = 'Purchase $24.00 or more and receive 20% off on your order';
+    var shippingDiscountMsg = 'Purchase $24.00 or more and receive Free Shipping with USPS (7-10 Business Days)';
+
+    it('1. should return a response containing promotional messages for the order and shipping discounts on PDP', function (done) {
+        myRequest.url = config.baseUrl + '/Product-Variation?pid='
+            + masterPid + '&dwvar_' + masterPid + '_color=BLUJEFB&quantity=1';
+        return request(myRequest, function (error, response) {
+            var bodyAsJson = JSON.parse(response.body);
+            assert.equal(response.statusCode, 200, 'Expected GET Product-Variation statusCode to be 200.');
+            assert.isTrue(bodyAsJson.product.promotions[0].calloutMsg === '20% off on your order');
+            assert.isTrue(bodyAsJson.product.promotions[1].calloutMsg === 'Free Shipping with USPS (7-10 Business Days)');
+            done();
+        });
+    });
+    it('2. should return a response containing approaching promotional call out messages', function (done) {
+        // add 2 product to Cart
+        myRequest.url = config.baseUrl + '/Cart-AddProduct?pid=' + variantPid + '&quantity=' + qty;
+        myRequest.method = 'POST';
+        return request(myRequest)
+            .then(function (response) {
+                assert.equal(response.statusCode, 200, 'expected add variant to Cart call to return status code 200');
+                var bodyAsJson = JSON.parse(response.body);
+                assert.isTrue(bodyAsJson.quantityTotal === 2, 'should have 2 items added to Cart');
+                assert.isTrue(bodyAsJson.message === 'Product added to basket');
+                cookieString = cookieJar.getCookieString(myRequest.url);
+            })
+            // select a shipping method with Form is required before going to Cart
+            .then(function () {
+                myRequest.form = myShippingMethodForm; // Ground Shipping method
+                myRequest.method = 'POST';
+                myRequest.url = config.baseUrl + '/Cart-SelectShippingMethod';
+                var cookie = request.cookie(cookieString);
+                cookieJar.setCookie(cookie, myRequest.url);
+                return request(myRequest);
+            })
+            .then(function (response) {
+                assert.equal(response.statusCode, 200, 'expected add shipping method to return status code 200');
+            })
+            // go to Cart needs to update the URL to use pretty url
+            .then(function () {
+                var NewUrl = config.baseUrl.replace('on/demandware.store/Sites-SiteGenesis-Site/', 's/SiteGenesis/cart?lang=');
+                var myNewRequest = {
+                    url: NewUrl,
+                    method: 'GET',
+                    rejectUnauthorized: false,
+                    resolveWithFullResponse: true,
+                    jar: cookieJar
+                };
+                var cookie = request.cookie(cookieString);
+                cookieJar.setCookie(cookie, myNewRequest.url);
+                return request(myNewRequest);
+            })
+            .then(function (response) {
+                assert.equal(response.statusCode, 200, 'expected cart page to return status code 200');
+                var $ = cheerio.load(response.body);
+                var discount = $('.single-approaching-discount');
+                assert.equal(discount.get(0).children[0].data.trim(), orderDiscountMsg);
+                assert.equal(discount.get(1).children[0].data.trim(), shippingDiscountMsg);
+                done();
+            });
+    });
+});
