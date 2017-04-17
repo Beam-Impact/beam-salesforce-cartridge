e80e802d81604d97eb8a309607fade86faa31dac
Zi Sardone <zsardone@salesforce.com>
2017-04-17 19:19:52 -0400

    RAP-5956-formValidation

--- test/functional/misc/formErrorValidation.js
+++ test/functional/misc/formErrorValidation.js
@@ -10,28 +10,36 @@ import * as loginPage from '../../mocks/testDataMgr/pageObjects/login';
 import * as homePage from '../../mocks/testDataMgr/pageObjects/home';
 
 describe('Login Page Create Account Form', () => {
-    before(() => {
-        return homePage.navigateTo()
-            .then(() => browser.isVisible(homePage.navBarButton))
+    before(() => homePage.navigateTo());
+
+    it('Validate that client side returned expected error when required fields are not filled', () => {
+        return browser.isVisible(homePage.navBarButton)
             .then(isVisible => {
                 if (isVisible) {
                     // access mobile devices
+                    // This test has been modified due to the appium 1.6.3 bug #7636
                     return browser.click(homePage.navBarButton)
                         .waitForVisible(homePage.navBar)
                         .click(homePage.signInButtonIpad)
                         .pause(3000)
-                        .click(loginPage.checkStatusButton);
-                        // this test has been modified because click CreateNewAccount Tab fails due to appium 1.6.3 issue #7636
+                        .click(loginPage.checkStatusButton)
+                        .elements(loginPage.getTrackOrderFormFeedback)
+                        .then(elements => {
+                            assert.equal(elements.value.length, 3, 'there should be 3 fields to fill');
+                            return elements.value.forEach(ele => browser.elementIdText(ele.ELEMENT)
+                                .then(msg => assert.equal(msg.value, 'value missing', 'expected error return to be value missing')))
+                        })
                 }
                 // access desktop browser
                 return browser.click(homePage.signInButton)
                     .click(loginPage.createAccountTab)
-                    .click(loginPage.createAccountButton);
+                    .click(loginPage.createAccountButton)
+                    .elements(loginPage.getRegisterFormFeedback)
+                    .then(elements => {
+                        assert.equal(elements.value.length, 7, 'there should be 7 fields to fill');
+                        return elements.value.forEach(ele => browser.elementIdText(ele.ELEMENT)
+                            .then(msg => assert.equal(msg.value, 'Please fill out this field.', 'expected erro return to be Please fill out this field.')))
+                    });
             });
-    });
-    it('Validate the expected error from the client side is displayed when form is not filled', () => {
-        return browser.elements(loginPage.getFormFeedback)
-            .then(elements => elements.value.forEach(ele => browser.elementIdText(ele.ELEMENT)
-                .then(msg => assert.equal(msg.value, 'Please fill out this field.', 'expected message is displayed'))));
     });
 });
--- test/mocks/testDataMgr/pageObjects/login.js
+++ test/mocks/testDataMgr/pageObjects/login.js
@@ -3,5 +3,6 @@
 export const createAccountTab = '.login-form-nav .nav-item:nth-child(2)';
 export const logInTab = '.login-form-nav .nav-item:nth-child(1)';
 export const createAccountButton = '#register .btn-primary';
-export const getFormFeedback = '#register .form-control-feedback';
+export const getRegisterFormFeedback = '#register .form-control-feedback';
+export const getTrackOrderFormFeedback = '.trackorder .form-control-feedback';
 export const checkStatusButton = '.trackorder .btn-primary';
