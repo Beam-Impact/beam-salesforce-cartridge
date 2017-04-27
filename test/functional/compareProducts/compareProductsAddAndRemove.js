f7ecc893e796e7ee270b93dd30590bee32fb7663
Zi Sardone <zsardone@salesforce.com>
2017-04-27 16:53:19 -0400

    RAP-6100-refactorCompareProducts

--- test/functional/compareProducts/compareProductsAddAndRemove.js
+++ test/functional/compareProducts/compareProductsAddAndRemove.js
@@ -6,24 +6,28 @@
  * - select 4 products to compare
  * - Verify the compare bar at the bottom of the page contains the 4 products
  * - Verify compare return the products expected.
+ * - Click on the Back To Results Page Button
+ * - Remove one product from the compareBar
+ * - Add a new product to the compareBar
+ * - Click on Compare button
+ * - Verify that there are 4 products in the Result page
  */
 
 import { assert } from 'chai';
-import { config } from '../webdriver/wdio.conf';
 import * as homePage from '../../mocks/testDataMgr/pageObjects/home';
 import * as keyboard from '../../mocks/testDataMgr/helpers/keyboard';
 import * as common from '../../mocks/testDataMgr/helpers/common';
 import * as search from '../../mocks/testDataMgr/pageObjects/searchResult';
 import * as compareProducts from '../../mocks/testDataMgr/pageObjects/compareProducts';
 import * as testDataMgr from '../../mocks/testDataMgr/main';
-import * as productTile from '../../mocks/testDataMgr/pageObjects/productTile';
 
 describe('Select 4 products for Compare', () => {
     const topTitle = '.page-title';
-    const selector1 = '[data-pid]:nth-child(1)' + ' ' + compareProducts.compareCheckbox;
-    const selector2 = '[data-pid]:nth-child(2)' + ' ' + compareProducts.compareCheckbox;
-    const selector3 = '[data-pid]:nth-child(3)' + ' ' + compareProducts.compareCheckbox;
-    const selector4 = '[data-pid]:nth-child(4)' + ' ' + compareProducts.compareCheckbox;
+    const selector1 = [compareProducts.dataPid + ':nth-child(1)', compareProducts.compareCheckbox].join(' ');
+    const selector2 = [compareProducts.dataPid + ':nth-child(2)', compareProducts.compareCheckbox].join(' ');
+    const selector3 = [compareProducts.dataPid + ':nth-child(3)', compareProducts.compareCheckbox].join(' ');
+    const selector4 = [compareProducts.dataPid + ':nth-child(4)', compareProducts.compareCheckbox].join(' ');
+    const selector5 = [compareProducts.dataPid + ':nth-child(5)', compareProducts.compareCheckbox].join(' ');
 
     before(() => testDataMgr.load()
         .then(() => homePage.navigateTo())
@@ -33,21 +37,25 @@ describe('Select 4 products for Compare', () => {
             if (isVisible) {
                 //  Access mobile devices
                 return browser.click(homePage.navBarButton)
-                    .waitForVisible(homePage.navBar)
+                    .pause(500)
+                    .waitForVisible(homePage.navBar, 5000)
                     .click(homePage.navWomenButton)
-                    .waitForVisible(homePage.navWomenClothingButton)
+                    .pause(500)
+                    .waitForVisible(homePage.navWomenClothingButton, 5000)
                     .click(homePage.navWomenClothingButton)
-                    .waitForVisible(homePage.navWomenClothingTopsButton)
+                    .pause(500)
+                    .waitForVisible(homePage.navWomenClothingTopsButton, 5000)
                     .click(homePage.navWomenClothingTopsButton)
-                    .waitForExist(homePage.navBarButton)
                     .pause(1000)
+                    .waitForExist(homePage.navBarButton, 5000)
                     .then(() => browser.getText(topTitle))
                     .then(title => assert.equal(title, 'Tops'))
                     .then(() => browser.click(selector1))
                     .click(selector2)
                     .click(selector3)
                     .click(selector4)
-                    .pause(2000);
+                    .waitForEnabled(compareProducts.buttonCompare, 5000)
+                    .pause(500);
             }
             //  Access desktop or laptop browsers
             return browser.click(search.searchForm)
@@ -75,8 +83,32 @@ describe('Select 4 products for Compare', () => {
     });
 
     it('should return 4 products at compare result page', () => {
-        return browser.click('button.compare')
-            .then(() => browser.pause(2000))
+        return browser.click(compareProducts.buttonCompare)
+            .pause(500)
+            .then(() => homePage.getProductTileCount())
+            .then(productCount => assert.isTrue(productCount === 4, 'Expected to have 4 product tiles on the compare result page'));
+    });
+
+    it('should be able to remove a product from compare table and add a new product', () => {
+        return browser.click(compareProducts.buttonGoBack)
+            .pause(500)
+            .elements(compareProducts.selectedProductImg)
+            .then(selectedProducts => assert.isTrue(selectedProducts.value.length === 4, 'there should be 4 products in compare bar'))
+            .then(() => browser.waitForVisible(compareProducts.selectorProdToRemoveIpad))
+            .then(() => common.getVisibleSelector(compareProducts.selectorProdToRemoveIpad, compareProducts.selectorProdToRemove))
+            .then(selector => browser.click(selector))
+            .pause(500)
+            .elements(compareProducts.selectedProductImg)
+            .then(selectedProducts => assert.isTrue(selectedProducts.value.length === 3, 'there should be 3 products in compare bar'))
+            .then(() => browser.click(selector5))
+            .then(() => browser.elements(compareProducts.selectedProductImg))
+            .then(prod => assert.isTrue(prod.value.length === 4, 'there should be 4 products in compare bar'));
+    });
+
+    it('should return 4 products at compare result page after remove and add', () => {
+        return browser.click(compareProducts.compareButton)
+            .pause(500)
+            .then(() => common.waitUntilPageLoaded())
             .then(() => homePage.getProductTileCount())
             .then(productCount => assert.isTrue(productCount === 4, 'Expected to have 4 product tiles on the compare result page'));
     });
--- test/mocks/testDataMgr/pageObjects/compareProducts.js
+++ test/mocks/testDataMgr/pageObjects/compareProducts.js
@@ -6,4 +6,7 @@ export const selectedProductImg = selectedProduct + ' img';
 export const compareButton = '.align-items-center .compare';
 export const close = '.close';
 export const dataPid = '[data-pid]';
-
+export const buttonGoBack = '.btn-outline-primary';
+export const buttonCompare = 'button[type="submit"]';
+export const selectorProdToRemove = [selectedProduct + ':nth-child(1)', close].join(' ');
+export const selectorProdToRemoveIpad = [selectedProduct + ':nth-child(1)', '.fa-close'].join(' ');
