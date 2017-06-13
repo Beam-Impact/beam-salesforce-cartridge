0f3532b70404620897f7722cbaa6192aee88c8ef
Zi Sardone <zsardone@salesforce.com>
2017-06-13 16:19:06 -0400

    fix variable unuser error

--- test/functional/cart/addVariantToCart.js
+++ test/functional/cart/addVariantToCart.js
@@ -12,7 +12,6 @@ describe('Cart - Add Variant To Cart', () => {
     let productVariationMaster;
     let resourcePath;
     let jenkins;
-    let jenkins2;
 
     const variant1 = {
         instance: undefined,
@@ -55,6 +54,7 @@ describe('Cart - Add Variant To Cart', () => {
                 const variant1Selection = new Map();
                 catalog = testDataMgr.parsedData.catalog;
                 const variantIds = productVariationMaster.getVariantProductIds();
+                jenkins = 'this is a test';
 
                 // No-Iron Textured Dress Shirt (Color: White, Size: 14 1/2, Width: 32/33)
                 variant1.instance = products.getProduct(catalog, variantIds[0]);
