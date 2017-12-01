pravngaur/developer_labs_old

w-4455687-bonusProducts modal redone
more button works

model works

cartDisplay part 1

linting round 1

ui for cart line items

can bring up modal from cart page with list and rule based bonus products

can now edit bonus products

update attaches new bonus item to pli

minicartUI

price is now being reflected to the bonus product promotion for both types of bonus products

cartridges/app_storefront_base/cartridge/models/productLineItem/decorators/index.js
@@ -12,5 +12,10 @@ module.exports = {
     quantityOptions: require('*/cartridge/models/productLineItem/decorators/quantityOptions'),
     options: require('*/cartridge/models/productLineItem/decorators/options'),
     quantity: require('*/cartridge/models/productLineItem/decorators/quantity'),
-    bundledProductLineItems: require('*/cartridge/models/productLineItem/decorators/bundledProductLineItems')
+    bundledProductLineItems: require('*/cartridge/models/productLineItem/decorators/bundledProductLineItems'),
+    bonusProductLineItemUUID: require('*/cartridge/models/productLineItem/decorators/bonusProductLineItemUUID'),
+    embededBonusProductLineItems: require('*/cartridge/models/productLineItem/decorators/embededBonusProductLineItems'),
+    embededDiscountBonusLineItems: require('*/cartridge/models/productLineItem/decorators/embededDiscountBonusLineItems'),
+    bonusUnitPrice: require('*/cartridge/models/productLineItem/decorators/bonusUnitPrice')
+
 };

Beam-Impact/beam-salesforce-cartridge

cartridges/app_storefront_base/cartridge/models/productLineItem/decorators/index.js
@@ -12,5 +12,10 @@ module.exports = {
     quantityOptions: require('*/cartridge/models/productLineItem/decorators/quantityOptions'),
     options: require('*/cartridge/models/productLineItem/decorators/options'),
     quantity: require('*/cartridge/models/productLineItem/decorators/quantity'),
-    bundledProductLineItems: require('*/cartridge/models/productLineItem/decorators/bundledProductLineItems')
+    bundledProductLineItems: require('*/cartridge/models/productLineItem/decorators/bundledProductLineItems'),
+    bonusProductLineItemUUID: require('*/cartridge/models/productLineItem/decorators/bonusProductLineItemUUID'),
+    preOrderUUID: require('*/cartridge/models/productLineItem/decorators/preOrderUUID'),
+    discountBonusLineItems: require('*/cartridge/models/productLineItem/decorators/discountBonusLineItems'),
+    bonusUnitPrice: require('*/cartridge/models/productLineItem/decorators/bonusUnitPrice')
+
 };