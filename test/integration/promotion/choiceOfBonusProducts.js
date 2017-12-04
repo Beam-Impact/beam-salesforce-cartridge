613ba41076285f64af914d9d43e620a0d0bf60dc
Alex Clark <alex.clark@salesforce.com>
2017-12-04 10:22:25 -0500

    integration test (list based bonus products)

--- test/integration/promotion/choiceOfBonusProducts.js
+++ test/integration/promotion/choiceOfBonusProducts.js
@@ -104,7 +104,7 @@ describe('Test Choice of bonus Products promotion Mini cart response.', function
     });
 });
 
-describe('Add Bonus Product to cart', function () {
+describe('Add rule base Bonus Product to cart', function () {
     this.timeout(45000);
 
     var variantPid1 = '701642842668';
@@ -185,6 +185,100 @@ describe('Add Bonus Product to cart', function () {
             var bodyAsJson = JSON.parse(response.body);
             assert.equal(bodyAsJson.action, 'Cart-AddBonusProducts');
             assert.equal(bodyAsJson.totalQty, 2);
+            assert.equal(bodyAsJson.msgSuccess, 'Bonus Products added to your cart');
+            assert.isTrue(bodyAsJson.success);
+            assert.isFalse(bodyAsJson.error);
+        });
+    });
+});
+
+describe('Add list base Bonus Product to cart', function () {
+    this.timeout(45000);
+
+    var variantPid1 = '013742002454';
+    var qty1 = 5;
+    var cookieJar = request.jar();
+    var myRequest = {
+        url: '',
+        method: 'POST',
+        rejectUnauthorized: false,
+        resolveWithFullResponse: true,
+        jar: cookieJar,
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+    };
+
+    var duuid;
+    var pliuuid;
+    var bonusChoiceRuleBased;
+    var bonuspids;
+
+    before(function () {
+        // ----- adding qualifying product #1:
+        myRequest.url = config.baseUrl + '/Cart-AddProduct';
+        myRequest.form = {
+            pid: variantPid1,
+            quantity: qty1
+        };
+
+        return request(myRequest)
+            .then(function (response) {
+                var bodyAsJson = JSON.parse(response.body);
+                duuid = bodyAsJson.newBonusDiscountLineItem.uuid;
+                bonusChoiceRuleBased = bodyAsJson.newBonusDiscountLineItem.bonusChoiceRuleBased;
+                pliuuid = bodyAsJson.newBonusDiscountLineItem.pliUUID;
+                bonuspids = bodyAsJson.newBonusDiscountLineItem.bonuspids;
+            });
+    });
+
+    it('It should reflect as a rule based bonus product', function () {
+        var bonuspidsExpected = [
+            '008885004540',
+            '008884304047',
+            '883360390116',
+            'pioneer-pdp-6010fd'
+        ];
+
+        assert.equal(bonusChoiceRuleBased, false);
+        assert.isNotNull(duuid);
+        assert.equal(bonuspids[0], bonuspidsExpected[0]);
+        assert.equal(bonuspids[1], bonuspidsExpected[1]);
+        assert.equal(bonuspids[2], bonuspidsExpected[2]);
+        assert.equal(bonuspids[3], bonuspidsExpected[3]);
+    });
+
+    it('bring up the edit bonus product window', function () {
+        myRequest.url = config.baseUrl + '/Cart-EditBonusProduct?duuid=' + duuid;
+        myRequest.form = {
+            duuid: duuid
+        };
+        myRequest.method = 'GET';
+        return request(myRequest)
+        .then(function (response) {
+            var bodyAsJson = JSON.parse(response.body);
+            assert.equal(bodyAsJson.action, 'Cart-EditBonusProduct');
+            assert.equal(bodyAsJson.addToCartUrl, '/on/demandware.store/Sites-MobileFirst-Site/en_US/Cart-AddBonusProducts');
+            assert.equal(bodyAsJson.showProductsUrl, '/on/demandware.store/Sites-MobileFirst-Site/en_US/Product-ShowBonusProducts');
+            assert.equal(bodyAsJson.maxBonusItems, 2);
+            assert.equal(bodyAsJson.pliUUID, pliuuid);
+            assert.equal(bodyAsJson.uuid, duuid);
+            assert.equal(bodyAsJson.bonusChoiceRuleBased, false);
+        });
+    });
+
+    it('Add Bonus Product to cart', function () {
+        var addBonusQueryString = '?pids={%22bonusProducts%22:[{%22pid%22:%22008885004540%22,%22qty%22:1,%22options%22:[null]}],%22totalQty%22:1}&uuid=' + duuid + '&pliuuid=' + pliuuid;
+        myRequest.url = config.baseUrl + '/Cart-AddBonusProducts' + addBonusQueryString;
+        myRequest.form = {
+            duuid: duuid
+        };
+        myRequest.method = 'POST';
+        return request(myRequest)
+        .then(function (response) {
+            var bodyAsJson = JSON.parse(response.body);
+            assert.equal(bodyAsJson.action, 'Cart-AddBonusProducts');
+            assert.equal(bodyAsJson.totalQty, 6);
             assert.equal(bodyAsJson.msgSuccess, 'Bonus Products added to your cart');
             assert.isTrue(bodyAsJson.success);
             assert.isFalse(bodyAsJson.error);
