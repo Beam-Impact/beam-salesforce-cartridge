bd36de3d3cf9ff940567e5a8e1299a8912600c98
Alex Clark <alex.clark@salesforce.com>
2017-12-05 08:35:57 -0500

    build error due to file name

--- cartridges/app_storefront_base/cartridge/client/js/default/cart.js
+++ cartridges/app_storefront_base/cartridge/client/js/default/cart.js
@@ -5,5 +5,5 @@ var processInclude = require('./util');
 $(document).ready(function () {
     processInclude(require('./cart/cart'));
     processInclude(require('./product/base'));
-    processInclude(require('./product/quickview'));
+    processInclude(require('./product/quickView'));
 });
