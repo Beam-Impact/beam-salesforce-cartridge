ab126cb32af8e3c0cf6a19f6855aed755faf07c8
Jing Wang <jingwang@salesforce.com>
2017-12-19 10:40:15 -0500

    PDP pages shows "null" when missing demo for promotion

--- cartridges/app_storefront_base/cartridge/models/product/decorators/promotions.js
+++ cartridges/app_storefront_base/cartridge/models/product/decorators/promotions.js
@@ -6,8 +6,8 @@ module.exports = function (object, promotions) {
         enumerable: true,
         value: promotions.length === 0 ? null : collections.map(promotions, function (promotion) {
             return {
-                calloutMsg: promotion.calloutMsg ? promotion.calloutMsg.markup : null,
-                details: promotion.details ? promotion.details.markup : null,
+                calloutMsg: promotion.calloutMsg ? promotion.calloutMsg.markup : '',
+                details: promotion.details ? promotion.details.markup : '',
                 enabled: promotion.enabled,
                 id: promotion.ID,
                 name: promotion.name,
