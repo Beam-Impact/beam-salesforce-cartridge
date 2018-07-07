b948399b2c69ba3c845cba45cc21bba8eff94b98
Ranveer Raghuwanshi <ranveer.raghuwanshi@clockwork.nl>
2018-07-07 19:22:10 +0200

    move internal private functions to productHelpers for easier override

--- cartridges/app_storefront_base/cartridge/scripts/factories/product.js
+++ cartridges/app_storefront_base/cartridge/scripts/factories/product.js
@@ -2,7 +2,6 @@
 
 var ProductMgr = require('dw/catalog/ProductMgr');
 var PromotionMgr = require('dw/campaign/PromotionMgr');
-var collections = require('*/cartridge/scripts/util/collections');
 var productHelper = require('*/cartridge/scripts/helpers/productHelpers');
 var productTile = require('*/cartridge/models/product/productTile');
 var bonusProduct = require('*/cartridge/models/product/bonusProduct');
@@ -16,146 +15,18 @@ var orderLineItem = require('*/cartridge/models/productLineItem/orderLineItem');
 var bonusOrderLineItem = require('*/cartridge/models/productLineItem/bonusOrderLineItem');
 var bundleOrderLineItem = require('*/cartridge/models/productLineItem/bundleOrderLineItem');
 
-
-/**
- * Return type of the current product
- * @param  {dw.catalog.ProductVariationModel} product - Current product
- * @return {string} type of the current product
- */
-function getProductType(product) {
-    var result;
-    if (product.master) {
-        result = 'master';
-    } else if (product.variant) {
-        result = 'variant';
-    } else if (product.variationGroup) {
-        result = 'variationGroup';
-    } else if (product.productSet) {
-        result = 'set';
-    } else if (product.bundle) {
-        result = 'bundle';
-    } else if (product.optionProduct) {
-        result = 'optionProduct';
-    } else {
-        result = 'standard';
-    }
-    return result;
-}
-
-/**
- * Normalize product and return Product variation model
- * @param  {dw.catalog.Product} product - Product instance returned from the API
- * @param  {Object} productVariables - variables passed in the query string to
- *                                     target product variation group
- * @return {dw.catalog.ProductVarationModel} Normalized variation model
- */
-function getVariationModel(product, productVariables) {
-    var variationModel = product.variationModel;
-    if (!variationModel.master && !variationModel.selectedVariant) {
-        variationModel = null;
-    } else if (productVariables) {
-        var variationAttrs = variationModel.productVariationAttributes;
-        Object.keys(productVariables).forEach(function (attr) {
-            if (attr && productVariables[attr].value) {
-                var dwAttr = collections.find(variationAttrs,
-                    function (item) { return item.ID === attr; });
-                var dwAttrValue = collections.find(variationModel.getAllValues(dwAttr),
-                    function (item) { return item.value === productVariables[attr].value; });
-                if (dwAttr && dwAttrValue) {
-                    variationModel.setSelectedAttributeValue(dwAttr.ID, dwAttrValue.ID);
-                }
-            }
-        });
-    }
-    return variationModel;
-}
-
-/**
- * Get information for model creation
- * @param {dw.catalog.Product} apiProduct - Product from the API
- * @param {Object} params - Parameters passed by querystring
- *
- * @returns {Object} - Config object
- */
-function getConfig(apiProduct, params) {
-    var variations = getVariationModel(apiProduct, params.variables);
-    if (variations) {
-        apiProduct = variations.selectedVariant || apiProduct; // eslint-disable-line
-    }
-
-    var promotions = PromotionMgr.activeCustomerPromotions.getProductPromotions(apiProduct);
-    var optionsModel = productHelper.getCurrentOptionModel(apiProduct.optionModel, params.options);
-    var options = {
-        variationModel: variations,
-        options: params.options,
-        optionModel: optionsModel,
-        promotions: promotions,
-        quantity: params.quantity,
-        variables: params.variables,
-        apiProduct: apiProduct,
-        productType: getProductType(apiProduct)
-    };
-
-    return options;
-}
-
-/**
- * Retrieve product's options and default selected values from product line item
- *
- * @param {dw.util.Collection.<dw.order.ProductLineItem>} optionProductLineItems - Option product
- *     line items
- * @param {string} productId - Line item product ID
- * @return {string []} - Product line item options
- */
-function getLineItemOptions(optionProductLineItems, productId) {
-    return collections.map(optionProductLineItems, function (item) {
-        return {
-            productId: productId,
-            optionId: item.optionID,
-            selectedValueId: item.optionValueID
-        };
-    });
-}
-
-/**
- * Retrieve product's options and default values
- *
- * @param {dw.catalog.ProductOptionModel} optionModel - A product's option model
- * @param {dw.util.Collection.<dw.catalog.ProductOption>} options - A product's configured options
- * @return {string []} - Product line item options
- */
-function getDefaultOptions(optionModel, options) {
-    return collections.map(options, function (option) {
-        var selectedValue = optionModel.getSelectedOptionValue(option);
-        return option.displayName + ': ' + selectedValue.displayValue;
-    });
-}
-
-/**
- * Retrieve product's options and default selected values from product line item
- *
- * @param {dw.util.Collection.<dw.order.ProductLineItem>} optionProductLineItems - Option product
- *     line items
- * @return {string[]} - Product line item option display values
- */
-function getLineItemOptionNames(optionProductLineItems) {
-    return collections.map(optionProductLineItems, function (item) {
-        return item.productName;
-    });
-}
-
 module.exports = {
     get: function (params) {
         var productId = params.pid;
         var apiProduct = ProductMgr.getProduct(productId);
-        var productType = getProductType(apiProduct);
+        var productType = productHelper.getProductType(apiProduct);
         var product = Object.create(null);
         var options = null;
         var promotions;
 
         switch (params.pview) {
             case 'tile':
-                product = productTile(product, apiProduct, getProductType(apiProduct));
+                product = productTile(product, apiProduct, productType);
                 break;
             case 'bonusProductLineItem':
                 promotions = PromotionMgr.activeCustomerPromotions.getProductPromotions(apiProduct);
@@ -164,7 +35,7 @@ module.exports = {
                     quantity: params.quantity,
                     variables: params.variables,
                     lineItem: params.lineItem,
-                    productType: getProductType(apiProduct)
+                    productType: productType
                 };
 
                 switch (productType) {
@@ -172,7 +43,7 @@ module.exports = {
                         // product = bundleProductLineItem(product, apiProduct, options, this);
                         break;
                     default:
-                        var variationsBundle = getVariationModel(apiProduct, params.variables);
+                        var variationsBundle = productHelper.getVariationModel(apiProduct, params.variables);
                         if (variationsBundle) {
                             apiProduct = variationsBundle.getSelectedVariant() || apiProduct; // eslint-disable-line
                         }
@@ -181,11 +52,11 @@ module.exports = {
                         var optionLineItemsBundle = params.lineItem.optionProductLineItems;
                         var currentOptionModelBundle = productHelper.getCurrentOptionModel(
                             optionModelBundle,
-                            getLineItemOptions(optionLineItemsBundle, productId)
+                            productHelper.getLineItemOptions(optionLineItemsBundle, productId)
                         );
                         var lineItemOptionsBundle = optionLineItemsBundle.length
-                            ? getLineItemOptionNames(optionLineItemsBundle)
-                            : getDefaultOptions(optionModelBundle, optionModelBundle.options);
+                            ? productHelper.getLineItemOptionNames(optionLineItemsBundle)
+                            : productHelper.getDefaultOptions(optionModelBundle, optionModelBundle.options);
 
 
                         options.variationModel = variationsBundle;
@@ -209,7 +80,7 @@ module.exports = {
                     quantity: params.quantity,
                     variables: params.variables,
                     lineItem: params.lineItem,
-                    productType: getProductType(apiProduct)
+                    productType: productType
                 };
 
                 switch (productType) {
@@ -222,7 +93,7 @@ module.exports = {
                         }
                         break;
                     default:
-                        var variationsPLI = getVariationModel(apiProduct, params.variables);
+                        var variationsPLI = productHelper.getVariationModel(apiProduct, params.variables);
                         if (variationsPLI) {
                             apiProduct = variationsPLI.getSelectedVariant() || apiProduct; // eslint-disable-line
                         }
@@ -231,11 +102,11 @@ module.exports = {
                         var optionLineItemsPLI = params.lineItem.optionProductLineItems;
                         var currentOptionModelPLI = productHelper.getCurrentOptionModel(
                             optionModelPLI,
-                            getLineItemOptions(optionLineItemsPLI, productId)
+                            productHelper.getLineItemOptions(optionLineItemsPLI, productId)
                         );
                         var lineItemOptionsPLI = optionLineItemsPLI.length
-                            ? getLineItemOptionNames(optionLineItemsPLI)
-                            : getDefaultOptions(optionModelPLI, optionModelPLI.options);
+                            ? productHelper.getLineItemOptionNames(optionLineItemsPLI)
+                            : productHelper.getDefaultOptions(optionModelPLI, optionModelPLI.options);
 
 
                         options.variationModel = variationsPLI;
@@ -253,7 +124,7 @@ module.exports = {
 
                 break;
             case 'bonus':
-                options = getConfig(apiProduct, params);
+                options = productHelper.getConfig(apiProduct, params);
 
                 switch (productType) {
                     case 'set':
@@ -267,7 +138,7 @@ module.exports = {
 
                 break;
             default: // PDP
-                options = getConfig(apiProduct, params);
+                options = productHelper.getConfig(apiProduct, params);
 
                 switch (productType) {
                     case 'set':
