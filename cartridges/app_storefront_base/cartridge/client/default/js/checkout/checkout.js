3dc37d62b575885f097cdcd1d632116d9ad0b70a
Syed Shehroz Hussain (Ozzy) <s.hussain@salesforce.com>
2019-05-31 15:45:58 -0400

    Bug: IE11 fieldset serialization issue fixed for billing page form which also fixes CSRF token mismatch issue (#491)

--- cartridges/app_storefront_base/cartridge/client/default/js/checkout/checkout.js
+++ cartridges/app_storefront_base/cartridge/client/default/js/checkout/checkout.js
@@ -171,7 +171,7 @@ var scrollAnimate = require('../components/scrollAnimate');
 
                     formHelpers.clearPreviousErrors('.payment-form');
 
-                    var billingAddressForm = $('#dwfrm_billing .billing-address-block').serialize();
+                    var billingAddressForm = $('#dwfrm_billing .billing-address-block :input').serialize();
 
                     $('body').trigger('checkout:serializeBilling', {
                         form: $('#dwfrm_billing .billing-address-block'),
@@ -183,7 +183,7 @@ var scrollAnimate = require('../components/scrollAnimate');
                         }
                     });
 
-                    var contactInfoForm = $('#dwfrm_billing .contact-info-block').serialize();
+                    var contactInfoForm = $('#dwfrm_billing .contact-info-block :input').serialize();
 
                     $('body').trigger('checkout:serializeBilling', {
                         form: $('#dwfrm_billing .contact-info-block'),
@@ -196,7 +196,7 @@ var scrollAnimate = require('../components/scrollAnimate');
                     });
 
                     var activeTabId = $('.tab-pane.active').attr('id');
-                    var paymentInfoSelector = '#dwfrm_billing .' + activeTabId + ' .payment-form-fields';
+                    var paymentInfoSelector = '#dwfrm_billing .' + activeTabId + ' .payment-form-fields :input';
                     var paymentInfoForm = $(paymentInfoSelector).serialize();
 
                     $('body').trigger('checkout:serializeBilling', {
