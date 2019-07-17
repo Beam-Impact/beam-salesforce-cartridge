f4af704d436a59b31670e469c0ebc2023de48da1
Tanveer Khan <tanveer.khan@salesforce.com>
2019-07-17 15:10:38 -0400

    Chore: Added not default to break point name(W-6354997) (#566)

--- cartridges/app_storefront_base/cartridge/controllers/Order.js
+++ cartridges/app_storefront_base/cartridge/controllers/Order.js
@@ -84,13 +84,10 @@ server.get(
         var OrderMgr = require('dw/order/OrderMgr');
         var OrderModel = require('*/cartridge/models/order');
         var Locale = require('dw/util/Locale');
-        var URLUtils = require('dw/web/URLUtils');
-
         var order;
         var validForm = true;
         var target = req.querystring.rurl || 1;
         var actionUrl = URLUtils.url('Account-Login', 'rurl', target);
-        
         var profileForm = server.forms.getForm('profile');
         profileForm.clear();
 
