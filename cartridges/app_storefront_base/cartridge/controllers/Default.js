e77752c732d39eb28224eb8491398c993ec40a35
Alex Clark <33070538+alex-clark-sfcc@users.noreply.github.com>
2019-10-28 09:41:52 -0400

    added to render the home page isml template in Default-Start, as that… (#729) (#738)
    * added to render the home page isml template in Default-Start, as that controller is called when sitepath is defined in the site aliases
    as per the documentation Was the matching hostName and sitePath (if any) found in the settings property?
    Yes - examine the remaining portion of the entered URL (the portion after the hostName and sitePath, if any) to determine the correct locale and nature of the requested resource (that is, to determine whether the URL specifies a category, content asset, or pipeline). If the entered URL specifies only the hostName and sitePath, or only the hostName, execute the Default-Start pipeline and return the response to the customer. Otherwise, retrieve the requested resource (executing a pipeline if necessary) and return the requested resource to the customer. If no locale is specified, use the default locale for the site when retrieving the requested resource. Skip all of the remaining steps in this process. - which is causing infinite redirect when combined with "entry-point-pipelines" : [ "Default-Start", "Home-Show" ]
    
    * fixed the controller start node

--- cartridges/app_storefront_base/cartridge/controllers/Default.js
+++ cartridges/app_storefront_base/cartridge/controllers/Default.js
@@ -2,12 +2,17 @@
 
 var server = require('server');
 var cache = require('*/cartridge/scripts/middleware/cache');
+var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
+var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
 
-server.get('Start', cache.applyDefaultCache, function (req, res, next) {
-    var URLUtils = require('dw/web/URLUtils');
-    res.redirect(URLUtils.url('Home-Show'));
+server.get('Start', consentTracking.consent, cache.applyDefaultCache, function (req, res, next) {
+    var Site = require('dw/system/Site');
+    var pageMetaHelper = require('*/cartridge/scripts/helpers/pageMetaHelper');
+
+    pageMetaHelper.setPageMetaTags(req.pageMetaData, Site.current);
+    res.render('/home/homePage');
     next();
-});
+}, pageMetaData.computedPageMetaData);
 
 /** Renders the maintenance page when a site has been set to "Maintenance mode" */
 server.get('Offline', cache.applyDefaultCache, function (req, res, next) {
