be4f70f262e3a23d3eef350396e7e7f210a3a025
Andreas Seyfarth <aseyfarth@salesforce.com>
2021-12-10 17:53:16 +0100

    fix for promotion cache issue W-7742735 (#1261)
    * fix for promotion cache issue W-7742735
    
    - fix and unit test for W-7742735
    
    * remove describe.only
    
    * linting
    
    * add comment on cache usage

--- cartridges/app_storefront_base/cartridge/scripts/util/promotionCache.js
+++ cartridges/app_storefront_base/cartridge/scripts/util/promotionCache.js
@@ -1,4 +1,4 @@
-/* globals session */
+/* globals session request */
 'use strict';
 
 var PromotionMgr = require('dw/campaign/PromotionMgr');
@@ -8,15 +8,25 @@ var promotionCache = Object.create(null);
 
 Object.defineProperty(promotionCache, 'promotions', {
     get: function () {
+        // will parse the id from the main request and use that as cache key for the promotion cache as the main goal is to ensure that
+        // the promotions are just calculated once for the main request (Search) and not all the remote includes.
+        // If a new main request comes in it should not be used as eventually applicable promotions have been changed linked to the user.
+        var cacheKey = request.requestID.split('-')[0];
         if (session.privacy.promoCache) {
-            return JSON.parse(session.privacy.promoCache);
+            var cacheObj = JSON.parse(session.privacy.promoCache);
+            if (cacheKey === cacheObj.cacheKey) {
+                return cacheObj.promoIds;
+            }
         }
         var activePromotions = PromotionMgr.activeCustomerPromotions.getProductPromotions();
         var promoIds = collections.map(activePromotions, function (promo) {
             return promo.ID;
         });
 
-        session.privacy.promoCache = JSON.stringify(promoIds);
+        session.privacy.promoCache = JSON.stringify({
+            cacheKey: cacheKey,
+            promoIds: promoIds
+        });
         return promoIds;
     }
 });
