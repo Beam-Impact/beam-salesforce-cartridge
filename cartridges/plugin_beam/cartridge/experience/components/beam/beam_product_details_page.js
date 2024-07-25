a34ee1731fc82a0ffd97ee03e91ddf7cb7998bcb
Myrr <108036576+myrr-BeamImpact@users.noreply.github.com>
2024-07-25 10:57:26 -0400

    SDK-2001: adding new pdp and subscription management widgets to sfcc cartridge (#3)
    * adding new pdp and subscription management widgets to sfcc cartridge
    
    * adding pdp widget to pdp page

---
+++ cartridges/plugin_beam/cartridge/experience/components/beam/beam_product_details_page.js
@@ -0,0 +1,18 @@
+'use strict';
+/* global response */
+
+var Template = require('dw/util/Template');
+var HashMap = require('dw/util/HashMap');
+
+/**
+ * Render logic for storefront.communityImpact component.
+ * @param {dw.experience.ComponentScriptContext} context The component script context object.
+ * @param {dw.util.Map} [modelIn] Additional model values created by another cartridge. This will not be passed in by Commerce Cloud Platform.
+ *
+ * @returns {string} The markup to be displayed
+ */
+module.exports.render = function (context, modelIn) {
+  var model = modelIn || new HashMap();
+
+  return new Template('beam/beam_product_details_page').render(model).text;
+};
