5e7e19542a198aa79a3fc959d88e0057216ab567
Myrr <108036576+myrr-BeamImpact@users.noreply.github.com>
2024-12-23 17:06:34 -0500

    SDK-2444: Adding support for language-location format translation (#7)
    * adding dynamic lang support and baseUrl configuration, ensuring consistant variable names
    
    * enabling default lang configuration
    
    * fixing baseUrl typo
    
    * removing logger
    
    * updating code to not utilize session
    
    * removing code comments

---
+++ cartridges/plugin_beam/cartridge/scripts/utils/getFinalLanguageCode.js
@@ -0,0 +1,29 @@
+/**
+ * This helper function checks if the current language is supported by Beam.
+ * If the current language is supported, it returns it, otherwise, it returns the default language.
+ *
+ * @param {dw.system.Site} site The current site object
+ * @param {string} currentLanguageCode The current language code (e.g., 'en', 'fr', etc.)
+ * @returns {string} The final language code
+ */
+function getFinalLanguageCode(site, currentLanguageCode) {
+    var supportedLangString =
+        site.getCustomPreferenceValue("beamSupportedLang");
+    var supportedLang = [];
+    try {
+        supportedLang = JSON.parse(supportedLangString);
+    } catch (e) {
+        supportedLang = [];
+    }
+
+    var defaultLang = site.getCustomPreferenceValue("beamDefaultLang");
+
+    // If current language is supported, use it; otherwise, fall back to default language
+    return supportedLang.indexOf(currentLanguageCode) !== -1
+        ? currentLanguageCode
+        : defaultLang;
+}
+
+module.exports = {
+    getFinalLanguageCode: getFinalLanguageCode,
+};
